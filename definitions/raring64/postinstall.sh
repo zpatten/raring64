1a9f3f869cbe73982f9bef96826a66a41772d608
Zachary Patten <zachary@jovelabs.com>
2014-01-23 14:54:38 -0800

    prime a few more key packages

--- definitions/raring64/postinstall.sh
+++ definitions/raring64/postinstall.sh
@@ -9,6 +9,7 @@ apt-get -y upgrade
 apt-get -y install linux-headers-$(uname -r) build-essential
 apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
 apt-get -y install vim
+apt-get -y install iptables-persistent pbzip2
 
 # Installing the virtualbox guest additions
 apt-get -y install dkms
@@ -28,14 +29,11 @@ cp /etc/sudoers /etc/sudoers.orig
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
 sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
 
-# Add puppet user and group
-# adduser --system --group --home /var/lib/puppet puppet
-
 # Install NFS client
 apt-get -y install nfs-common
 
 # Prime LXC
-apt-get -y install lxc lxc-templates
+apt-get -y install lxc lxc-templates yum
 
 lxc-create -n seed -t ubuntu -- --release lucid
 lxc-destroy -n seed -f
@@ -45,37 +43,6 @@ lxc-destroy -n seed -f
 
 lxc-create -n seed -t ubuntu -- --release trusty
 lxc-destroy -n seed -f
-
-apt-get clean
-
-# Install Ruby from source in /opt so that users of Vagrant
-# can install their own Rubies using packages or however.
-# wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p286.tar.gz
-# tar xvzf ruby-1.9.3-p286.tar.gz
-# cd ruby-1.9.3-p286
-# ./configure --prefix=/opt/ruby
-# make
-# make install
-# cd ..
-# rm -rf ruby-1.9.3-p286
-# rm ruby-1.9.3-p286.tar.gz
-
-# Install RubyGems 1.8.24
-# wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.24.tgz
-# tar xzf rubygems-1.8.24.tgz
-# cd rubygems-1.8.24
-# /opt/ruby/bin/ruby setup.rb
-# cd ..
-# rm -rf rubygems-1.8.24
-# rm rubygems-1.8.24.tgz
-
-# Installing chef & Puppet
-# /opt/ruby/bin/gem install chef --no-ri --no-rdoc
-# /opt/ruby/bin/gem install puppet --no-ri --no-rdoc
-
-# Add /opt/ruby/bin to the global path as the last resort so
-# Ruby, RubyGems, and Chef/Puppet are visible
-# echo 'PATH=$PATH:/opt/ruby/bin/'> /etc/profile.d/vagrantruby.sh
 
 # Installing vagrant keys
 mkdir /home/vagrant/.ssh
@@ -88,6 +55,7 @@ chown -R vagrant /home/vagrant/.ssh
 # Remove items used for building, since they aren't needed anymore
 apt-get -y remove linux-headers-$(uname -r) build-essential
 apt-get -y autoremove
+apt-get -y clean
 
 # Zero out the free space to save space in the final image:
 dd if=/dev/zero of=/EMPTY bs=1M
